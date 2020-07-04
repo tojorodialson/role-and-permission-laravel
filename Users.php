09daad3cee46cba101608b33b8d03b61071b7683
Tojo RODIALSON <tojorodialson@gmail.com>
2020-07-04 14:53:11 +0300

    Create users model

---
+++ Users.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Traits\HasPermissionsTrait;
+
+class Users extends Authenticatable {
+
+    use Notifiable, HasPermissionsTrait;
+
+    protected $table = 'users';
+
+    protected $keyType = 'string';
+
+    protected $fillable = ['username', 'email', 'password', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password', 'code', 'remember_token', 'token'];
+
+    public $incrementing = false;
+}
